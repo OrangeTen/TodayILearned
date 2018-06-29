e8d313a03625eaf8ecc351c3180586639605691f
이종구 <addy94@naver.com>
2018-06-29 21:16:30 +0900

    make user create, read

--- tilup-server/api/user.js
+++ tilup-server/api/user.js
@@ -1,3 +1,26 @@
-module.exports = {
+const User = require('mongoose').model('User');
 
+module.exports = {
+    add(req, res, next) {
+        const user = new User(req.body);
+        user.save(err => {
+            if (err) return console.log(err);
+            res.json(user);
+        });
+    },
+    get(req, res, next) {
+        User.find()
+            .exec((err, users)=>{
+                if(err) return console.log(err);
+                res.json(users);
+            });
+    },
+    getOne(req, res, next) {
+        User.findById(req.params.userId)
+            .exec((err, user)=>{
+                if(err) return console.log(err);
+                if(!user) return console.log("no user");
+                res.json(user);
+            });
+    }
 };
