a898ed254ca787fcce72f308a0cdec0759838d54
kujyp <pjo901018@gmail.com>
2018-06-30 01:40:40 +0900

    Fix firebase auth

---
+++ tilup-web/src/utils/firebaseUtils.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+
+let _initialized = false;
+export function initializeFirebase() {
+  if (!_initialized) {
+    console.log("firebase initialize");
+    let config = {
+      apiKey: "AIzaSyAiZt3uJqSbvTphwgOdllefmKy2Qfs0ZiA",
+      authDomain: "tilu-1c341.firebaseapp.com",
+      databaseURL: "https://tilu-1c341.firebaseio.com",
+      projectId: "tilu-1c341",
+      storageBucket: "tilu-1c341.appspot.com",
+      messagingSenderId: "25009930917"
+    };
+    firebase.initializeApp(config);
+    _initialized = true;
+  }
+}
