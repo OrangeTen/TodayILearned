10f315196ed6e387d9bb2c71d3c56b80d1abff43
Jay Jin <jayjinjay@gmail.com>
2018-06-30 04:35:23 +0900

    Add easter egg

--- tilup-web/src/components/NavigationBar.js
+++ tilup-web/src/components/NavigationBar.js
@@ -6,12 +6,27 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 // import * as firebase from 'firebase'
 import firebase from 'firebase'
+import logo from './logo.png'
+import realLogo from './real_logo.png'
+import startEasterEgg from '../utils/startEasterEgg'
 
 export default class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      easterCount: 10,
+      optionList: [
+        { text: "Inbox" },
+        { text: "JavaScript" },
+        { text: "Interview Questions" },
+      ]
+    };
+    
+  }
+
   handleLogin() {
     var provider = new firebase.auth.GithubAuthProvider();
     console.log("login", provider);
-    
 
     firebase.auth().signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
@@ -28,15 +43,34 @@ export default class NavigationBar extends Component {
       // ...
     });
   }
+
+  handleEasterEgg = () => {
+    // this.setState({easterCount: this.state.easterCount - 1})
+    this.setState(function(prevState, props){
+      return {easterCount: prevState.easterCount -1}
+   });
+    if(this.state.easterCount <= 1) {
+      startEasterEgg();
+    }
+  }
+
   render() {
     return (
       <AppBar position="sticky" color="default" className="navigation-bar">
         <Toolbar>
+          {
+            this.state.easterCount <= 0? 
+            <img src={realLogo} className="logo" /> :
+              <img src={logo} className="logo" style={{opacity: this.state.easterCount/10}} onClick={this.handleEasterEgg}/> 
+          }
           <Typography variant="title" color="inherit" className="navigation-bar__title">
-          TILUP 🍊
+          TILUP
           </Typography>
           <Button color="inherit" onClick={this.handleLogin}>Login with GitHub</Button>
         </Toolbar>
+        <div style={{display: "none"}}>
+          <img id="source" src={realLogo} width="300" height="227" />
+        </div>
       </AppBar>
     );
   }
