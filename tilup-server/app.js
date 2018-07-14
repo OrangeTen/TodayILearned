67fa757dc85fd9274312581206cdecf4232d758c
kujyp <pjo901018@gmail.com>
2018-07-15 01:53:53 +0900

    Add Authorization on Access-Control-Allow-Headers

--- tilup-server/app.js
+++ tilup-server/app.js
@@ -21,7 +21,7 @@ app.use(methodOverride());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
     return res.status(200).json({});
