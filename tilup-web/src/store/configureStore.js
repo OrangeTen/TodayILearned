4485923699a7d5d743ba138258108040fef37d33
kujyp <pjo901018@gmail.com>
2018-07-18 16:49:25 +0900

    Divide configureStore into .dev, .prod / Add redux-logger as dev-dependency / Enable Redux-ChromeDevtool / Add REACT_EDITOR on .env.sample

--- tilup-web/src/store/configureStore.js
+++ tilup-web/src/store/configureStore.js
@@ -1,12 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers';
-import ReduxThunk from 'redux-thunk';
-
-const createAppStore = compose(
-	applyMiddleware(ReduxThunk)
-)(createStore);
-
-export default function configureStore(initialState){
-	const store = createAppStore(rootReducer, initialState);
-	return store;
+if ( process.env.NODE_ENV === 'production' ) {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
 }
