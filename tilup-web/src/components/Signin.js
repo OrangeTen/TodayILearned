56806fc4346f3415ad8e377e5e3a840904501435
Kyoungsik.Dev <kyoungsik.dev@gmail.com>
2018-07-22 14:37:37 +0900

    Enable signin btn in signin component

--- tilup-web/src/components/Signin.js
+++ tilup-web/src/components/Signin.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { connect } from "react-redux";
 
 import logo from '../components/logo.png'
+import { firebasePopupGithubSignin } from "../actions/firebase";
 
-
-const Signin = () => (
+const Signin = (props) => (
   <div className="pleaseLogin">
     <img src={logo} alt="pleaseLoginLogo" className="pleaseLogin__logo" />
-    <Button variant="extendedFab" color="#000" className="pleaseLogin__btn" onClick={this.handleLogin}>
+    <Button variant="extendedFab" color="#000" className="pleaseLogin__btn" onClick={props.firebasePopupGithubSignin}>
       <img src="/res/octocat.svg" alt="octocat" className="pleaseLogin__icon" />
       Login with GitHub
     </Button>
   </div>
 );
 
-export default Signin;
+const mapDispatchToProps = {
+  firebasePopupGithubSignin
+};
+
+
+export default connect(()=>{}, mapDispatchToProps)(Signin);
+
