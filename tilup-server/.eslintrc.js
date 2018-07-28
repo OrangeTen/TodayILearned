de31784524059f4a8dbd907c00e4aedb24b9b34d
kujyp <pjo901018@gmail.com>
2018-07-28 23:47:58 +0900

    Update eslint rules (#105)

--- tilup-server/.eslintrc.js
+++ tilup-server/.eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-    extends: "airbnb-base",
-    rules: {
-        'no-console': 'off',
-    },
+  extends: "airbnb-base",
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': [
+      "error", {
+        "allow": ["_id", ],
+      },
+    ],
+    'no-unused-vars': ["error", {
+      "argsIgnorePattern": "^_",
+    }],
+    "no-use-before-define": ["error", {
+      "functions": false,
+      "classes": true
+    }]
+  },
 };
